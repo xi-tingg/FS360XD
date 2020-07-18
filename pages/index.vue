2575bf0ae04b0f0e27baef13d5677d61dce244a7
xi-tingg <63148489+xi-tingg@users.noreply.github.com>
2020-07-18 11:23:29 +0800

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<script>
+    import Logo from '~/components/items/Logo.vue'
+    import InputElement from "~/components/items/Input.vue";
+    export default {
+        components: {
+            Logo,
+            InputElement
+        },
+        data: function(){
+            return{
+                email: "",
+                password: ""
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="flex h-screen w-full bg-gray-700">
+        <div class="m-auto w-full px-5 md:w-1/3">
+            <div class="pl-1 text-white font-medium text-2xl tracking-wide text-center">
+                INVENTORY SYSTEM {{ email }}
+            </div>
+                <form>
+                    <div class="my-2">
+                        <inputElement
+                            :labelName="'EMAIL'"
+                            :labelId="'email'"
+                            :inputType="'email'"
+                            :model="email"
+                            @input="email = $event"
+                            :disabled="false"
+                        /> 
+                    </div>
+                    <div class="my-2">
+                        <inputElement
+                            :labelName="'PASSWORD'"
+                            :labelId="'password'"
+                            :inputType="'password'"
+                            :model="password"
+                            @input="password = $event"
+                            :disabled="false"
+                        />
+                    </div>
+                    <button type="button" class="
+                        transition
+                        duration-500
+                        ease-in-out
+                        bg-blue-600
+                        rounded-xs
+                        w-full
+                        py-3
+                        text-white
+                        font-sans
+                        font-bold
+                        tracking-wide
+                        hover:bg-blue-700
+                        hover:text-gray-400
+                        active:outline-none
+                        focus:outline-none
+                        "> SIGN IN 
+                    </button>
+                </form>
+            
+        </div>
+    </div>
+</template>
+
+
